7d518aba27fd4a1adf30ecf8a20a601bb9eed5d5
Yogesh2061 <ybhandari461@gmail.com>
2024-06-07 01:26:17 +0545

    Java Script mini Practice Projects

---
+++ Insta reaction effect/script.js
@@ -0,0 +1,15 @@
+var container = document.querySelector("#container")
+var love = document.querySelector("i")
+
+container.addEventListener("dblclick", function(){
+    love.style.transform = ' translate(-50%,-50%) scale(1)'
+    love.style.opacity = 0.8
+
+ setTimeout(function(){
+        love.style.opacity = 0
+    },1000)
+    setTimeout(function(){
+        love.style.transform = ' translate(-50%,-50%) scale(0)'
+    },2000)
+})
+
